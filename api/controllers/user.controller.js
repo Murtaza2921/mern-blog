e72adaf8c70fd84c58137a8983d3cf4b14e2897e
ghulam murtaza <asif.raza@bytedash.my>
2024-02-16 17:58:16 +0500

    api for profile update

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,7 @@
-export const test =  (req, res) => {
-    res.json({ message: "API is working!" });
-  };
+export const test = (req, res) => {
+  res.json({ message: "API is working!" });
+};
+
+export const userUpdate = async (req, res, next) => {
+  console.log(req.user);
+};
