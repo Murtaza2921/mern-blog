65a0cb40808af2fae94528c97bf311a9777877c2
ghulam murtaza <asif.raza@bytedash.my>
2024-02-19 12:10:25 +0500

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { test, userUpdate } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  test,
+  userUpdate,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, userUpdate);
-
+router.delete("/delete/:userId", verifyToken, deleteUser);
 export default router;
