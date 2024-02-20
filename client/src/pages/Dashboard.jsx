561d64acd36af1b7876f9d4ad67fd60cdaf67d52
ghulam murtaza <asif.raza@bytedash.my>
2024-02-20 15:19:47 +0500

    show user posts inside dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState("");
@@ -22,7 +23,7 @@ export default function Dashboard() {
       {tab === "profile" && <DashProfile />}
 
       {/* posts... */}
-      {tab === 'posts' && <DashPosts/>}
+      {tab === "posts" && <DashPosts />}
     </div>
   );
 }
